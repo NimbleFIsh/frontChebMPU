6d2cd087c83efa212c4f9f005174e05301574b9c
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-29 20:43:03 +0300

    change start loc

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let map = L.map('map').setView([56.1265,47.2440], 13);
+    let map = L.map('map').setView([56.1012, 47.2261], 12);
     L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map); // Задание слоя маски - обычный
 });

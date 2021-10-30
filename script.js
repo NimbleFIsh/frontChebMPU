65a9aff2cd10f9d02daf85168cbb95bf22b4b001
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-30 22:08:20 +0300

    comments and refactors for nice

--- script.js
+++ script.js
@@ -11,75 +11,85 @@ function sendReq(method, mode, callback, postData = '', id) { // Функция 
     xhr.send(JSON.stringify(postData));
 }
 
-const getMarkers = () => Array.from(document.getElementsByClassName('leaflet-marker-pane')[0].children);
-const removeContextPin = () => getMarkers().forEach(img => img.src.includes(userMarker) && img.remove());
-const clearMarkers = () => getMarkers().forEach(el => el.remove());
+const getMarkers = () => Array.from(document.getElementsByClassName('leaflet-marker-pane')[0].children); // Получение списка всех маркеров
+const removeContextPin = () => getMarkers().forEach(img => img.src.includes(userMarker) && img.remove()); // Очистка метки пользователя
+const clearMarkers = () => getMarkers().forEach(el => el.remove()); // Очистка всей карты
 
-function createCategory(element) {
+function createCategory(element, container = 'modeList') { // Наполняет выпадающий список категориями
     const option = document.createElement('option');
     option.id=element.id;
     option.innerText=element.name;
-    // radio.dataset.color = element.color;
-    document.getElementById('modeList').insertAdjacentElement('beforeend', option);
+    option.dataset.color = element.color;
+    document.getElementById(container).insertAdjacentElement('beforeend', option);
 }
 
 const renderForm = () => document.getElementById('container').insertAdjacentHTML('beforeend', '<div id="form"><div id="closeForm"></div><label for="description">Укажите тему обращения</label><textarea id="description"></textarea><label for="text">Опишите вашу проблему</label><textarea id="text"></textarea><div id="attention">Укажите на карте току, нажав правой кнопкой мыши</div><div id="sendForm">Отправить</div></div>');
 
 document.addEventListener('DOMContentLoaded', () => {
-    let coords = [];
-    let map = L.map('map').setView([56.1012, 47.2261], 12);
+    let coords; // Дамп для координат
+    let map = L.map('map').setView([56.1012, 47.2261], 12); // Инициализация карты
     L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map); // Задание слоя маски - обычный
-    map.on('contextmenu', () => null);
+    map.on('contextmenu', () => null); // Отключение стандартного контекстного меню браузера
 
     const setMarker = (coords, content, icon=markersParh + marker, size) => L.marker(coords, {icon:L.icon({iconUrl:icon,iconSize:size,iconAnchor:[5,35],popupAnchor:[0,-40]})}).bindPopup(content).addTo(map);
 
-    const contextMenu = e => {
-        removeContextPin();
-        coords = e.latlng;
-        setMarker(e.latlng, 'Ваша точка', markersParh + userMarker, [25, 40]);
-        document.getElementById('attention').innerHTML = '<span id="unselectPin">Убрать маркер</span>';
-        document.getElementById('unselectPin').addEventListener('click', () => {
-            removeContextPin();
+    const contextMenu = e => { // 
+        removeContextPin(); // Удаление точки пользователя
+        
+        coords = e.latlng; // Дамп координат
+        
+        setMarker(e.latlng, 'Ваша точка', markersParh + userMarker, [25, 40]); // Установка точки пользователя
+        
+        document.getElementById('attention').innerHTML = '<span id="unselectPin">Убрать маркер</span>'; // Рендер отмены выбора точки
+        document.getElementById('unselectPin').addEventListener('click', () => { // Обработчик отмены выбора точки
+            removeContextPin(); // Удаление точки пользователя
             document.getElementById('attention').innerText = 'Укажите на карте току, нажав правой кнопкой мыши';
         });
     }
 
-    const openForm = () => {
-        if (!document.getElementById('form')) {
-            renderForm();
-            map.addEventListener('contextmenu', contextMenu);
+    const openForm = () => { // Открытие формы
+        if (!document.getElementById('form')) { // Если форма не была открыта ранее
+            renderForm(); // Рендер формы
+
+            map.addEventListener('contextmenu', contextMenu); // Обработчик контекстного меню
+
             document.getElementById('closeForm').addEventListener('click', e => {
                 e.target.parentElement.remove();
                 map.removeEventListener('contextmenu', contextMenu);
-                removeContextPin();
+                removeContextPin(); // Удаление точки пользователя
             });
-            document.getElementById('sendForm').addEventListener('click', () => {
-                const description = document.getElementById('description');
-                const text = document.getElementById('text');
-                if (description.value !== '' && text.value !== '' && (coords.lng && coords.lat))
-                    sendReq('POST', 'createRequest', () => console.log('Успешно создано!'),
-                        { "summary": description.value, "text": text.value, "coordinate": { "lon": coords.lng, "lat": coords.lat } });
+
+            document.getElementById('sendForm').addEventListener('click', () => { // Обработчик отправки формы
+                const description = document.getElementById('description'); // Получение полей формы для чтения их значений
+                const text = document.getElementById('text'); // Получение полей формы для чтения их значений
+
+                if (description.value !== '' && text.value !== '' && (coords.lng && coords.lat)) // Отправка формы, только если она полностью заполнена
+                    sendReq('POST', 'createRequest', () => alert('Успешно создано!'), { "summary": description.value, "text": text.value, "coordinate": { "lon": coords.lng, "lat": coords.lat } });
                 else alert('Пожалуйста заполните все поля и поставте точку на карте');
-                coords = [];
+                coords = []; // Сброс координат
             });
         }
     }
     
     const renderSettings = () => {
-        document.body.insertAdjacentHTML('afterbegin', '<div id="container"><ul id="settings"></ul></div>');
-        sendReq('GET', 'categories', data => {
+        document.body.insertAdjacentHTML('afterbegin', '<div id="container"><ul id="settings"></ul></div>'); // Рендер базовой структуры
+
+        sendReq('GET', 'categories', data => { // Запрос категорий
             const select = document.createElement('select');
             select.id="modeList";
             select.addEventListener('change', e => sendReq('GET', 'points', data => console.log(data), null, e.target.selectedOptions[0].id));
-            document.getElementById('container').insertAdjacentElement('afterbegin', select);
-            data.forEach(createCategory);
-            sendReq('GET', 'points', data => console.log(data), null, 1);
-            document.getElementById('settings').insertAdjacentHTML('beforeend', '<div id="openForm">Отправить запрос</div>');
-            document.getElementById('openForm').addEventListener('click', openForm);
+
+            document.getElementById('container').insertAdjacentElement('afterbegin', select); // Рендер контейнера списка категорий
+            data.forEach(createCategory); // Наполнение списка категорий
+
+            sendReq('GET', 'points', data => console.log(data), null, 1); // Запрос категории по id
+
+            document.getElementById('settings').insertAdjacentHTML('beforeend', '<div id="openForm">Отправить запрос</div>'); // Рендер кнопки открытия формы
+            document.getElementById('openForm').addEventListener('click', openForm); // Обработчик открытия формы
         });
     }
 
-    renderSettings();
+    renderSettings(); // Точка входа
     
-    setMarker([56.1012, 47.2261], 'Текст'); // Тестовый маркет
+    setMarker([56.1012, 47.2261], 'Текст'); // Тестовый маркер
 });

1940d88f1e1b2e1ede8be2c04e03445777f9ef0f
NimbleFIsh <karpov2001ura@mail.ru>
2021-10-30 20:57:18 +0300

    fix networking

--- script.js
+++ script.js
@@ -60,12 +60,12 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('sendForm').addEventListener('click', () => {
                 const description = document.getElementById('description');
                 const text = document.getElementById('text');
-                console.log({ "summary": description.value, "text": text.value, "coordinate": { "lon": coords.lng, "lat": coords.lat } });
-                if (description.value !== '' && text.value !== '' && (coords.lng && coords.lat)) alert('Пожалуйста заполните все поля и поставте точку на карте');
-                // else sendReq('POST', 'createRequest', data => {
-                //     console.log(data);
-                //     if (data.id) console.log('Успешно создано!');
-                // }, { "summary": description.value, "text": text.value, "coordinate": { "lon": coords.lng, "lat": coords.lat } });
+                if (description.value !== '' && text.value !== '' && (coords.lng && coords.lat))
+                    sendReq('POST', 'createRequest', data => {
+                        console.log(data);
+                        if (data.id) console.log('Успешно создано!');
+                    }, { "summary": description.value, "text": text.value, "coordinate": { "lon": coords.lng, "lat": coords.lat } });
+                else alert('Пожалуйста заполните все поля и поставте точку на карте');
                 coords = [];
             });
         }
